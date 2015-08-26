f7bf8340400fde5c82d8175e868884024ec9a669
Andrew Chin <achin@eminence32.net>
2015-08-25 23:11:02 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+
+
+#[cfg(unix)]
+extern crate libc;
+#[cfg(unix)]
+mod unix;
+#[cfg(unix)]
+pub use unix::terminal_size;
+
+
+
+
+#[test]
+fn it_works() {
+    let x = terminal_size();
+    println!("{:?}", x);
+}
---
+++ src/unix.rs
@@ -0,0 +1,36 @@
+extern crate libc;
+use std::os::raw::*;
+
+
+const TIOCGWINSZ: c_int = 0x00005413;
+
+#[derive(Debug)]
+struct WinSize {
+    ws_row: c_ushort,
+    ws_col: c_ushort,
+    ws_xpixel: c_ushort,
+    ws_ypixel: c_ushort
+}
+
+pub fn terminal_size() -> Option<(u16, u16)> {
+    use libc::{isatty, STDIN_FILENO};
+    use libc::funcs::bsd44::ioctl;
+    let is_tty: bool = unsafe{isatty(STDIN_FILENO) == 1};
+
+    if !is_tty { return None; }
+
+    let (rows, cols) = unsafe {
+        let mut winsize = WinSize{ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0};
+        ioctl(STDIN_FILENO, TIOCGWINSZ, &mut winsize);
+        println!("{:?}", winsize);
+        let rows = if winsize.ws_row > 0 { winsize.ws_row } else { 0 };
+        let cols = if winsize.ws_col > 0 { winsize.ws_col } else { 0 };
+        (rows as u16, cols as u16)
+    };
+
+    if rows > 0 && cols > 0 {
+        Some((rows, cols))
+    } else {
+        None
+    }
+}
