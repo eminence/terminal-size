d1a2cdcf74fdc83616f37fb7ef76942ab52fc7ec
Andrew Chin <achin@eminence32.net>
2015-09-07 10:09:22 -0400

    Use stdout instead of stdin when trying to get size (unix)
    v0.1.2

--- src/unix.rs
+++ src/unix.rs
@@ -15,17 +15,17 @@ struct WinSize {
 
 /// Returns the size of the terminal, if available.
 ///
-/// If STDIN is not a tty, returns `None`
+/// If STDOUT is not a tty, returns `None`
 pub fn terminal_size() -> Option<(Width, Height)> {
-    use self::libc::{isatty, STDIN_FILENO};
+    use self::libc::{isatty, STDOUT_FILENO};
     use self::libc::funcs::bsd44::ioctl;
-    let is_tty: bool = unsafe{isatty(STDIN_FILENO) == 1};
+    let is_tty: bool = unsafe{isatty(STDOUT_FILENO) == 1};
 
     if !is_tty { return None; }
 
     let (rows, cols) = unsafe {
         let mut winsize = WinSize{ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0};
-        ioctl(STDIN_FILENO, TIOCGWINSZ, &mut winsize);
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut winsize);
         let rows = if winsize.ws_row > 0 { winsize.ws_row } else { 0 };
         let cols = if winsize.ws_col > 0 { winsize.ws_col } else { 0 };
         (rows as u16, cols as u16)
