64912dd4be0fcd7967e3279afebdc40c54d8e32e
Lzu Tao <taolzu@gmail.com>
2019-12-26 23:50:33 +0700

    Remove some redundant checks

--- src/unix.rs
+++ src/unix.rs
@@ -1,64 +1,39 @@
-extern crate libc;
-use std::os::raw::*;
-use std::os::unix::io::RawFd;
-
 use super::{Height, Width};
-
-#[cfg(target_os = "macos")]
-const TIOCGWINSZ: c_ulong = 0x40087468;
-#[cfg(all(target_env = "musl", not(target_os = "macos")))]
-const TIOCGWINSZ: c_int = 0x00005413;
-#[cfg(all(not(target_env = "musl"), not(target_os = "macos")))]
-const TIOCGWINSZ: c_ulong = 0x00005413;
-
-#[derive(Debug)]
-struct WinSize {
-    ws_row: c_ushort,
-    ws_col: c_ushort,
-    ws_xpixel: c_ushort,
-    ws_ypixel: c_ushort,
-}
+use std::os::unix::io::RawFd;
 
 /// Returns the size of the terminal defaulting to STDOUT, if available.
 ///
 /// If STDOUT is not a tty, returns `None`
 pub fn terminal_size() -> Option<(Width, Height)> {
-    use self::libc::STDOUT_FILENO;
-    terminal_size_using_fd(STDOUT_FILENO)
+    terminal_size_using_fd(libc::STDOUT_FILENO)
 }
 
 /// Returns the size of the terminal using the given file descriptor, if available.
 ///
 /// If the given file descriptor is not a tty, returns `None`
 pub fn terminal_size_using_fd(fd: RawFd) -> Option<(Width, Height)> {
-    use self::libc::ioctl;
-    use self::libc::isatty;
+    use libc::ioctl;
+    use libc::isatty;
+    use libc::{winsize as WinSize, TIOCGWINSZ};
     let is_tty: bool = unsafe { isatty(fd) == 1 };
 
     if !is_tty {
         return None;
     }
 
-    let (rows, cols) = unsafe {
-        let mut winsize = WinSize {
-            ws_row: 0,
-            ws_col: 0,
-            ws_xpixel: 0,
-            ws_ypixel: 0,
-        };
-        ioctl(fd, TIOCGWINSZ, &mut winsize);
-        let rows = if winsize.ws_row > 0 {
-            winsize.ws_row
-        } else {
-            0
-        };
-        let cols = if winsize.ws_col > 0 {
-            winsize.ws_col
-        } else {
-            0
-        };
-        (rows as u16, cols as u16)
+    let mut winsize = WinSize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
     };
+
+    if unsafe { ioctl(fd, TIOCGWINSZ, &mut winsize) } != -1 {
+        return None;
+    }
+
+    let rows = winsize.ws_row;
+    let cols = winsize.ws_col;
 
     if rows > 0 && cols > 0 {
         Some((Width(cols), Height(rows)))
